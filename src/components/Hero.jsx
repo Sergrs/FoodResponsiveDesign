b95eff5d7fe420cc95690aa47ffd84a31e85cd5d
Serg <sergeyrs@bk.ru>
2022-08-28 06:06:59 +0700

    initial commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div>
+      <div>
+        {/* Overlay */}
+        <div>
+          <h1>
+            The<span>Best</span>
+          </h1>
+          <h1>
+            Foods<span>Delivery</span>
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
